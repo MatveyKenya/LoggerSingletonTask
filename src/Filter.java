282fd0370c784a5014e00c2aa65f205e30088510
Matvey Kenya <matveykenya@gmail.com>
2021-08-28 21:29:51 +0500

    #1

--- src/Filter.java
+++ src/Filter.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Filter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source) {
+        Logger logger = Logger.getInstance();
+        List<Integer> result = new ArrayList<>();
+        //..
+        return result;
+    }
 }
--- src/Logger.java
+++ src/Logger.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Logger {
+public class Logger {
+    private static Logger logger;
+    protected int num = 1;
+
+    private Logger() {}
+
+    public void log(String msg) {
+        System.out.println("[" + num++ + "] " + msg);
+    }
+
+    public static Logger getInstance() {
+        return logger == null
+                ? logger = new Logger()
+                : logger;
+    }
 }
